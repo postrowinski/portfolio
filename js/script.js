76aa928d1e866a096bc2d4fd8fba3e5b92512b60
Przemek <p.ostrowinski@gmail.com>
2017-06-26 15:55:53 +0200

    add contact and fix nav

--- js/script.js
+++ js/script.js
@@ -1,4 +1,13 @@
 $(document).ready(function () {
+    'use strict';
+    
+    //Defane function animation for navigation links
+    function scrollTopAnimation(top) {
+        $('body').animate({
+            scrollTop: top
+        }, 500);
+    }
+    
     //Show and Hide navigation section
     $('#toggle').click(function () {
         $(this).toggleClass('active');
@@ -8,4 +17,16 @@ $(document).ready(function () {
     //Adding Tooltip
     $('.skills-icon, .future-skills-icon').attr('data-toggle', 'tooltip').attr('data-placement', 'bottom');
     $('[data-toggle="tooltip"]').tooltip();
+    
+    //By click on nav links moving to the right clickec section with amination and close active menu
+    $('.nav-links').click(function (e) {
+        e.preventDefault();
+        
+        var getLink = $(this).attr('href'),
+            gotTop = $(getLink).offset().top;
+        
+        scrollTopAnimation(gotTop);
+        $('#overlay').removeClass('open');
+        $('#toggle').removeClass('active');
+    });
 });
