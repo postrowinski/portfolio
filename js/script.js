003b78c16e56e8226c802bdc1fbd3330e349fb31
Przemek <p.ostrowinski@gmail.com>
2017-06-26 12:45:58 +0200

    optymalize img

--- js/script.js
+++ js/script.js
@@ -1,8 +1,11 @@
 $(document).ready(function () {
+    //Show and Hide navigation section
     $('#toggle').click(function () {
         $(this).toggleClass('active');
         $('#overlay').toggleClass('open');
     });
-    $('.skills-icon').attr('data-toggle', 'tooltip').attr('data-placement', 'bottom');
-     $('[data-toggle="tooltip"]').tooltip();
+    
+    //Adding Tooltip
+    $('.skills-icon, .future-skills-icon').attr('data-toggle', 'tooltip').attr('data-placement', 'bottom');
+    $('[data-toggle="tooltip"]').tooltip();
 });
